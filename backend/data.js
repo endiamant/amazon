7c430142ac80c8b0325cac3858f1067896373369
123 <a123@MacBook-Air-Anela.local>
2020-11-25 18:38:56 +0600

    second commit

---
+++ backend/data.js
@@ -0,0 +1,43 @@
+export default {
+  products: [
+    {
+      _id: "123",
+      name: "Slim Shirt",
+      category: "Shirts",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      _id: "124",
+      name: "Slim Shirt",
+      category: "Shirts",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+    },
+
+    {
+      name: "Slim Shirt",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+    },
+
+    {
+      _id: "125",
+      name: "Dress",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+    },
+  ],
+};
